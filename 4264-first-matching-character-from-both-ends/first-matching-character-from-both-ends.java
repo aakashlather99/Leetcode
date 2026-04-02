b298ef6aacd67376542d324738ecd21633c17529
aakashlather99 <aakashlather20@gmail.com>
2026-04-02 21:57:49 +0530

    Time: 1 ms (27.54%) | Memory: 43.3 MB (80.88%) - LeetSync

---
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int n = s.length();
+        int limit = (n - 1) / 2;
+        
+        for (int i = 0; i <= limit; i++) {
+            if (s.charAt(i) == s.charAt(n - i - 1)) {
+                return i; 
+            }
+        }
+        return -1;
+    }
+}
